d949dfe03455ba0ae57d17363eb2181158ad3224
Riady Santoso <gikiet82@gmail.com>
2021-01-27 09:57:33 +0700

    Updates

---
+++ precache-manifest.38f4906be5944efd3c4188cd173752dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3308d350717f2d90c4693e6b04313c67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a147124ae0bf20fa9b67",
+    "url": "./static/css/2.7ca58fb4.chunk.css"
+  },
+  {
+    "revision": "2cc7445c2b83aff5ddad",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "a147124ae0bf20fa9b67",
+    "url": "./static/js/2.64c6d9dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.64c6d9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc7445c2b83aff5ddad",
+    "url": "./static/js/main.f7c56190.chunk.js"
+  },
+  {
+    "revision": "a6b2560807bac7dd6109e9a007c7bea5",
+    "url": "./static/js/main.f7c56190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192f8eb337f1341efb4e",
+    "url": "./static/js/runtime-main.4e9bbe03.js"
+  }
+]);
